ba302a60523223d367013ca68b30db11990e7b7e
Nikita T <nikitataldykin3156@yandex.ru>
2023-07-21 17:19:58 +0300

    Constants have been removed to a separate file

---
+++ src/consts.js
@@ -0,0 +1,11 @@
+export const fetchState ={
+    BLANC: 'BLANC',
+    LOADING: 'LOADING',
+    SUCCESS: 'SUCCESS',
+    ERROR: 'ERROR'
+}
+
+export const answerPlayer = {
+    RIGHT_ANSWER: 'This is the RIGHT answer!',
+    WRONG_ANSWER: 'This is the WRONG answer!'
+}
